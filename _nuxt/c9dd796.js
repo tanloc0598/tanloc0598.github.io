d8806382d4e79632fc4a289502a21ad016657b2d
Tan Loc <tnloc@Outlook.com>
2021-12-06 02:36:36 +0700

    update titls

---
+++ _nuxt/c9dd796.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{255:function(t,e,n){"use strict";n.r(e);var c=n(18),component=Object(c.a)({},(function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"tw-text-center tw-text-sm"},[n("span",[t._v(t._s(t.$config.footer))])])}),[],!1,null,"b3bcd69e",null);e.default=component.exports}}]);
